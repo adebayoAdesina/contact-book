0ed14402b896f30de9941ea5fa3f82feca981d33
Adebayo Adesina <adebayoadesina789@gmail.com>
2025-05-07 23:27:34 +0100

    Updating the backend.

--- contact-book-api/src/main/java/com/contact/book/Contact.java
+++ contact-book-api/src/main/java/com/contact/book/Contact.java
@@ -1,9 +1,15 @@
 package com.contact.book;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 public class Contact {
@@ -15,7 +21,7 @@ public class Contact {
     private String email;
     private String phone;
 
-    // Getters and Setters
+
     public Long getId() {
         return id;
     }
--- contact-book-api/src/main/java/com/contact/book/ContactRepository.java
+++ contact-book-api/src/main/java/com/contact/book/ContactRepository.java
@@ -1,6 +1,8 @@
 package com.contact.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
